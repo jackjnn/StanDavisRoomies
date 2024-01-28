9130e02c959a553d10220a2c96cc29bf5e8b189f
Jack Njoroge <jacknyangenj.jn2@gmail.com>
2024-01-28 15:10:19 -0500

    chainge main.ts

--- party/main.ts
+++ party/main.ts
@@ -1,5 +1,12 @@
 import type * as Party from "partykit/server";
+import ChatRoomsServer from "./chatRooms";
 
-export default class PartyServer {
-  constructor(public party: Party.Party) {}
-}
+// export default class PartyServer {
+//   constructor(public party: Party.Party) {}
+// }
+
+export default (party: Party.Party) => {
+  console.log("PartyServer starting");
+  new ChatRoomsServer(party);
+  console.log("ChatRoomsServer instantiated");
+};
